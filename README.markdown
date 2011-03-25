a61be190f1d06753b61e5208df4300509844b95f
Alexandre Testu <atestu@gmail.com>
2011-03-25 14:32:17 -0400

    updated README (image), added explanation of the numbers, and fixed the oil price (caused error 500)

--- README.markdown
+++ README.markdown
@@ -5,8 +5,10 @@ TCOMStock is a single serving webapp written in Ruby using [Sinatra](http://www.
 
 Its only purpose is to display 3 financial indicators:
 
+* The value of 1 euro in USD,
 * The price of an ounce of gold,
-* The value of 1 euro in USD
 * The price of an oil barrel.
+
+![](http://cl.ly/5V1Y/Screen_shot_2011-03-25_at_2.30.14_PM.png)
 
 This simple site may save your life (or give you a good grade) in class with [M. Stéphan](http://fr.linkedin.com/pub/alain-stephan/0/7b/154).
--- tcom.rb
+++ tcom.rb
@@ -7,7 +7,7 @@ require 'haml'
 get '/' do
   docEuro = open("http://www.boursorama.com/cours.phtml?symbole=1xEURUS") { |f| Hpricot(f) }
   docGold = open("http://www.boursorama.com/cours.phtml?symbole=1xGLDAM") { |f| Hpricot(f) }
-  docOil = open("http://www.boursorama.com/cours.phtml?symbole=CLOC10") { |f| Hpricot(f) }
+  docOil = open("http://www.boursorama.com/cours.phtml?symbole=CLINTN") { |f| Hpricot(f) }
   @euro = (docEuro/"#cours_Header_0 div.body div.FVHead div.FVInfB div.InfB span.gras").first.inner_html.to_f
   @gold = (docGold/"#cours_Header_0 div.body div.FVHead div.FVInfB div.InfB span.gras").first.inner_html.gsub(/ /,"").to_i
   @oil = (docOil/"#cours_Header_0 div.body div.FVHead div.FVInfB div.InfB span.gras").first.inner_html.to_f
